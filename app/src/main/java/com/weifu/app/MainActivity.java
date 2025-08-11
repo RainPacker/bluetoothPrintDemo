3f7d5bf35ee83be990ea7b3d677fd85c807eba57
yangyang.zhang <yangyang.zhang@weifu.com.cn>
2025-08-11 09:43:17 +0800

    fix：更新优化配置

--- app/src/main/java/com/weifu/app/MainActivity.java
+++ app/src/main/java/com/weifu/app/MainActivity.java
@@ -108,11 +108,11 @@ public class MainActivity extends AppCompatActivity /**implements Scanner.DataLi
     String TAG = getClass().getSimpleName();
     // prod
 //    private static final String LOADRL ="http://10.1.4.141:81/" ;
-//    private static final String LOADRL ="http://10.204.10.28:31338/" ;
+    private static final String LOADRL ="http://10.204.10.28:31338/" ;
 //    private static final String LOADRL ="http://10.1.77.103/" ;
 
 //    private static final String LOADRL ="http://10.1.4.145" ;
-    private static final String LOADRL ="file:///android_asset/test.html" ;
+//    private static final String LOADRL ="file:///android_asset/test.html" ;
 //    private static final String LOADRL ="http://10.94.31.150:31223/" ;
     private WebView webView;
     private final int PICK_REQUEST = 10011;
